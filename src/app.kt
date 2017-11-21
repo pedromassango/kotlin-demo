be285cfc7f36fc7dbf24f2b149da9ae0df19ae49
pedromassango <peter3fashion@gmail.com>
2017-11-21 12:14:39 +0100

    Initial commit

---
+++ src/app.kt
@@ -0,0 +1,15 @@
+
+
+fun main(args: Array<String>){
+
+    soma(4, 6)
+
+}
+
+fun soma(x: Int, y: Int){
+    val resultado = x + y
+
+    //println( "Soma igual a: " +resultado)
+
+    println( "Soma igual a: $resultado")
+}
