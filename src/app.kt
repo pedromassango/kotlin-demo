68ea63520ea224f81a29fcc1c0524a4d50c441b5
pedromassango <peter3fashion@gmail.com>
2017-11-22 01:06:51 +0100

    projeto calculadora aula 3

--- src/app.kt
+++ src/app.kt
@@ -2,14 +2,24 @@
 
 fun main(args: Array<String>){
 
-    soma(4, 6)
+    val PI = 3.14
+
+    //val PI = 3.14
+
+    var idade = 20
+
+
+    println("O valor de PI = $PI")
+    println("Eu tenho: $idade")
+    //soma(4, 6)
 
 }
 
+/*
 fun soma(x: Int, y: Int){
     val resultado = x + y
 
     //println( "Soma igual a: " +resultado)
 
     println( "Soma igual a: $resultado")
-}
+}*/
---
+++ src/calculadora.kt
@@ -0,0 +1,24 @@
+
+fun main(args: Array<String>){
+
+    val X = 11
+    val Y = 2
+
+    val resultado = multiplica(X, Y)
+
+    println("O resultado da soma é = $resultado")
+}
+
+/*
+    Metodo para somar dois numeros.
+ */
+fun soma(x: Int, y:Int) = x+y
+
+/*
+    Metodo multiplicador de dois numeros.
+ */
+fun multiplica(x: Int, y: Int) = x*y
+
+fun dividir(x: Int, y: Int) = x/y
+
+fun subtrair(x: Int, y: Int) = x-y
